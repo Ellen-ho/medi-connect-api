72b350098743034216aa3f52aec5b3c9c379db2e
Ellen <ellen31725@gmail.com>
2023-04-21 13:06:59 +0800

    feat: modify the type of exerciseRecord

--- src/infrastructure/adapters/ExerciseRecord/ExerciseRecord.ts
+++ src/infrastructure/adapters/ExerciseRecord/ExerciseRecord.ts
@@ -18,16 +18,16 @@ export class ExerciseRecordEntity {
   @Column({ name: 'exercise_date' })
   public exerciseDate!: Date
 
-  @Column({ name: 'exercise_type', type: 'jsonb' })
+  @Column({ name: 'exercise_type', type: 'varchar', length: 100 })
   public exerciseType!: ExerciseType
 
   @Column({ name: 'duration', type: 'number', length: 50 })
   public duration!: number
 
-  @Column({ name: 'intensity', type: 'jsonb' })
+  @Column({ name: 'intensity', type: 'varchar', length: 100 })
   public intensity!: IntensityType
 
-  @Column({ name: 'calories_burned', type: 'number', length: 20 })
+  @Column({ name: 'calories_burned', type: 'number', length: 50 })
   public caloriesBurned!: number
 
   @CreateDateColumn({ name: 'created_at' })
