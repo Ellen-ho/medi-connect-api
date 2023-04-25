7d5436713fc19cea88719f56f3ad8d9b34122458
Ellen <ellen31725@gmail.com>
2023-04-25 13:10:22 +0800

    feat: modify patient route

--- src/infrastructure/http/routes/PatientRoutes.ts
+++ src/infrastructure/http/routes/PatientRoutes.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express'
 import { IPatientController } from '../controllers/PatientController'
 import { asyncHandler } from '../middlewares/AsyncHandler'
-import { authenticated } from '../middlewares/Auth'
 export class PatientRoutes {
   private readonly routes: Router
   constructor(private readonly patientController: IPatientController) {
@@ -9,12 +8,10 @@ export class PatientRoutes {
     this.routes
       .post(
         '/profile',
-        authenticated,
         asyncHandler(this.patientController.createPatientProfile)
       )
       .patch(
         '/profile',
-        authenticated,
         asyncHandler(this.patientController.editPatientProfile)
       )
   }
