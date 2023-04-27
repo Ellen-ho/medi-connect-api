3eb385bbf3a001d88176089bf17ec394007d9233
Ellen <ellen31725@gmail.com>
2023-04-28 00:56:58 +0800

    feat: add function to answer agreement repository interface

--- src/domain/question/interfaces/repositories/IAnswerAgreementRepository.ts
+++ src/domain/question/interfaces/repositories/IAnswerAgreementRepository.ts
@@ -4,5 +4,10 @@ export interface IAnswerAgreementRepository {
   findById: (id: string) => Promise<AnswerAgreement | null>
   countsByAnswerId: (id: string) => Promise<number>
   findAllByAnswerId: (id: string, take?: number) => Promise<AnswerAgreement[]>
+  findByIdAndAgreedDoctorId: (
+    answerAgreementId: string,
+    agreedDoctorId: string
+  ) => Promise<AnswerAgreement | null>
+  deleteById: (id: string) => Promise<void>
   save: (answerAgreement: AnswerAgreement) => Promise<void>
 }
