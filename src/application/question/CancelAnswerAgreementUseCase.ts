b196ea6fbeb2e7afa7a2a50d40de1e72e8bcc129
Ellen <ellen31725@gmail.com>
2023-04-28 00:45:57 +0800

    feat: add cancel function of answer agreement usecase

---
+++ src/application/question/CancelAnswerAgreementUseCase.ts
@@ -0,0 +1,63 @@
+import { IDoctorRepository } from '../../domain/doctor/interfaces/repositories/IDoctorRepository'
+import { IAnswerAgreementRepository } from '../../domain/question/interfaces/repositories/IAnswerAgreementRepository'
+import { User } from '../../domain/user/User'
+
+interface CancelAnswerAgreementRequest {
+  user: User
+  answerAgreementId: string
+}
+
+interface CancelAnswerAgreementResponse {
+  totalAgreedDoctorCounts: number
+  agreedDoctorAvatars: Array<string | null>
+}
+
+export class CancelAnswerAgreementUseCase {
+  constructor(
+    private readonly answerAgreementRepository: IAnswerAgreementRepository,
+    private readonly doctorRepository: IDoctorRepository
+  ) {}
+
+  public async execute(
+    request: CancelAnswerAgreementRequest
+  ): Promise<CancelAnswerAgreementResponse> {
+    const { user, answerAgreementId } = request
+
+    const existingDoctor = await this.doctorRepository.findByUserId(user.id)
+
+    if (existingDoctor == null) {
+      throw new Error('Doctor does not exist.')
+    }
+
+    const existingAnswerAgreement =
+      await this.answerAgreementRepository.findByIdAndAgreedDoctorId(
+        answerAgreementId,
+        existingDoctor.id
+      )
+    if (existingAnswerAgreement == null) {
+      throw new Error('Answer agreement does not exist.')
+    }
+
+    await this.answerAgreementRepository.deleteById(existingAnswerAgreement.id)
+
+    const totalAgreedDoctorCounts =
+      await this.answerAgreementRepository.countsByAnswerId(
+        existingAnswerAgreement.answer.id
+      )
+
+    const lastAnswerAgreements =
+      await this.answerAgreementRepository.findAllByAnswerId(
+        existingAnswerAgreement.answer.id,
+        3
+      )
+
+    const agreedDoctorAvatars = lastAnswerAgreements.map(
+      (answerAgreement) => answerAgreement.agreedDoctor.avatar
+    )
+
+    return {
+      totalAgreedDoctorCounts,
+      agreedDoctorAvatars,
+    }
+  }
+}
