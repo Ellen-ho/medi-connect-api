d3ca63d4f9ce099e9a012bd6a865f91743d1dd6b
Ellen <ellen31725@gmail.com>
2023-05-22 23:24:23 +0800

    feat:add facebook and google login

--- src/infrastructure/config/passportConfig.ts
+++ src/infrastructure/config/passportConfig.ts
@@ -1,17 +1,26 @@
 import passport from 'passport'
 import { Strategy as LocalStrategy } from 'passport-local'
 import { Strategy as JWTStrategy, ExtractJwt } from 'passport-jwt'
+import { Strategy as FacebookStrategy } from 'passport-facebook'
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20'
 import { IUserRepository } from '../../domain/user/interfaces/repositories/IUserRepository'
 import bcrypt from 'bcrypt'
 
 import dotenv from 'dotenv'
+import { User, UserRoleType } from '../../domain/user/User'
+import { IUuidService } from '../../domain/utils/IUuidService'
 
 dotenv.config()
 
 export class PassportConfig {
-  constructor(private readonly userRepo: IUserRepository) {
+  constructor(
+    private readonly userRepo: IUserRepository,
+    private readonly uuidService: IUuidService
+  ) {
     this.initializeLocalStrategy()
     this.initializeJwtStrategy()
+    this.initializeFacebookStrategy()
+    this.initializeGoogleStrategy()
   }
 
   private initializeLocalStrategy(): void {
@@ -76,6 +85,129 @@ export class PassportConfig {
             done({ error }, false)
           })
       })
+    )
+  }
+
+  // private initializeFacebookStrategy(): void {
+  //   passport.use(
+  //     new FacebookStrategy(
+  //       {
+  //         clientID: process.env.FACEBOOK_ID,
+  //         clientSecret: process.env.FACEBOOK_SECRET,
+  //         callbackURL: process.env.FACEBOOK_CALLBACK,
+  //         profileFields: ['email', 'displayName'],
+  //       },
+
+  //       (accessToken, refreshToken, profile, done) => {
+  //         this.userRepo
+  //           .findByEmail({ email: profile._json.email })
+  //           .then((user) => {
+  //             if (user !== null) {
+  //               done(null, user)
+  //               return
+  //             }
+  //           })
+  //   const { name, email } = profile._json
+  //   this.userRepo
+  //     .findByEmail(email)
+  //     .then((user) => {
+  //       if (user == null) {
+  //         const error = new Error('驗證失敗！')
+  //         done(error)
+  //         return
+  //       }
+
+  //       const randomPassword = Math.random().toString(36).slice(-8)
+  //       bcrypt
+  //         .genSalt(10)
+  //         .then((salt) => bcrypt.hash(randomPassword, salt))
+  //         .then((hash) =>
+  //           this.userRepo.createUser({
+  //             name,
+  //             email,
+  //             password: hash,
+  //           })
+  //         )
+  //         .then((createdUser) => done(null, createdUser))
+  //         .catch((err) => done(err, false))
+  //     })
+  //     .catch((err) => done(err, false))
+  //       }
+  //     )
+  //   )
+  // }
+  private initializeFacebookStrategy(): void {
+    passport.use(
+      new FacebookStrategy(
+        {
+          clientID: process.env.FACEBOOK_ID as string,
+          clientSecret: process.env.FACEBOOK_SECRET as string,
+          callbackURL: process.env.FACEBOOK_CALLBACK as string,
+          profileFields: ['email', 'displayName'],
+        },
+
+        (accessToken, refreshToken, profile, done) => {
+          const { email, displayName } = profile._json
+          this.userRepo
+            .findByEmail(profile._json.email)
+            .then((user) => {
+              if (user !== null) {
+                done(null, user)
+                return
+              }
+
+              const randomPassword = Math.random().toString(36).slice(-8)
+              bcrypt
+                .genSalt(10)
+                .then((salt) => bcrypt.hash(randomPassword, salt))
+                .then((hash) => {
+                  const user = new User({
+                    id: this.uuidService.generateUuid(),
+                    displayName,
+                    email,
+                    hashedPassword: randomPassword,
+                    role: UserRoleType.PATIENT,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                  })
+
+                  await this.userRepo.save(user)
+                })
+                .then((createdUser) => done(null, createdUser))
+                .catch((err) => done(err, false))
+            })
+            .catch((err) => {
+              done(err)
+            })
+        }
+      )
+    )
+  }
+
+  private initializeGoogleStrategy(): void {
+    passport.use(
+      new GoogleStrategy(
+        {
+          clientID: process.env.GOOGLE_ID as string,
+          clientSecret: process.env.GOOGLE_SECRET as string,
+          callbackURL: process.env.GOOGLE_CALLBACK as string,
+        },
+
+        (accessToken, refreshToken, profile, done) => {
+          this.userRepo
+            .findById(profile.id)
+            .then((user) => {
+              if (user !== null) {
+                done(null, user)
+              } else {
+                done(new Error('User not found'))
+              }
+            })
+            .catch((err) => {
+              done(err)
+            })
+        }
+      )
     )
   }
 }
