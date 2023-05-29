23e4b55845d8578a39a4c607e333297e31b4b88f
Ellen <ellen31725@gmail.com>
2023-05-29 23:07:54 +0800

    fix: modify the parameter name of id

--- src/application/goal/GoalValidator.ts
+++ src/application/goal/GoalValidator.ts
@@ -1,13 +1,13 @@
 import Joi from 'joi'
 
 export const activateHealthGoalSchema = {
-  params: Joi.object({ healthGoalId: Joi.string().uuid().required() }),
+  params: Joi.object({ id: Joi.string().uuid().required() }),
 }
 
 export const rejectHealthGoalSchema = {
-  params: Joi.object({ healthGoalId: Joi.string().uuid().required() }),
+  params: Joi.object({ id: Joi.string().uuid().required() }),
 }
 
 export const getHealthGoalSchema = {
-  params: Joi.object({ healthGoalId: Joi.string().uuid().required() }),
+  params: Joi.object({ id: Joi.string().uuid().required() }),
 }
