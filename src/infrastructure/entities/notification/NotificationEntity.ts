f16fb8cec6fcfa8bb132af9bf07a5ba2dbd59317
Ellen <ellen31725@gmail.com>
2023-06-09 00:29:41 +0800

    feat: add notification entity,mapper,repository

---
+++ src/infrastructure/entities/notification/NotificationEntity.ts
@@ -0,0 +1,48 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  RelationId,
+  UpdateDateColumn,
+} from 'typeorm'
+import { NotificationType } from '../../../domain/notification/Notification'
+import { UserEntity } from '../user/UserEntity'
+
+@Entity('notifications')
+export class NotificationEntity {
+  @PrimaryGeneratedColumn('uuid')
+  public id!: string
+
+  @Column({ name: 'is_read', type: 'boolean', default: false })
+  public isRead!: boolean
+
+  @Column({ name: 'title', type: 'varchar', length: 100 })
+  public title!: string
+
+  @Column({ name: 'content', type: 'varchar', length: 250 })
+  public content!: string
+
+  @Column({ name: 'notification_type', type: 'string', length: 250 })
+  public notificationType!: NotificationType
+
+  @CreateDateColumn({ name: 'created_at' })
+  public createdAt!: Date
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  public updatedAt!: Date
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  public deletedAt!: Date
+
+  @ManyToOne(() => UserEntity, { nullable: false })
+  @JoinColumn({ name: 'user_id' })
+  public user!: UserEntity
+
+  @Column({ name: 'user_id' })
+  @RelationId((notification: NotificationEntity) => notification.user)
+  public userId!: string
+}
---
+++ src/infrastructure/entities/notification/NotificationRepository.ts
@@ -0,0 +1,104 @@
+import { DataSource } from 'typeorm'
+import {
+  Notification,
+  NotificationType,
+} from '../../../domain/notification/Notification'
+import { INotificationRepository } from '../../../domain/notification/interfaces/repositories/INotificationRepository'
+import { BaseRepository } from '../../database/BaseRepository'
+import { NotificationEntity } from './NotificationEntity'
+import { NotificationMapper } from './NotificationMapper'
+import { RepositoryError } from '../../error/RepositoryError'
+
+export class NotificationRepository
+  extends BaseRepository<NotificationEntity, Notification>
+  implements INotificationRepository
+{
+  constructor(dataSource: DataSource) {
+    super(NotificationEntity, new NotificationMapper(), dataSource)
+  }
+
+  public async findByIdAndUserId(
+    notificationId: string,
+    userId: string
+  ): Promise<Notification | null> {
+    try {
+      const entity = await this.getRepo().findOne({
+        where: {
+          id: notificationId,
+          user: { id: userId }, // need to set @RelationId
+        },
+      })
+      return entity != null ? this.getMapper().toDomainModel(entity) : null
+    } catch (e) {
+      throw new RepositoryError(
+        'NotificationRepository findByIdAndUserId error',
+        e as Error
+      )
+    }
+  }
+
+  public async findByUserIdAndCountAll(
+    userId: string,
+    limit: number,
+    offset: number
+  ): Promise<{
+    total_counts: number
+    notifications: Array<{
+      id: string
+      title: string
+      isRead: boolean
+      notificationType: NotificationType
+      createdAt: Date
+      updatedAt: Date
+    }>
+  }> {
+    try {
+      const rawNotifications = await this.getQuery<
+        Array<{
+          total_counts: number
+          id: string
+          title: string
+          is_read: boolean
+          notification_type: NotificationType
+          created_at: Date
+          updated_at: Date
+        }>
+      >(
+        `
+          SELECT
+            (SELECT COUNT(*) FROM notifications WHERE user_id = $1) as total_counts,
+            id,
+            title,
+            is_read,
+            notification_type,
+            created_at,
+            updated_at
+          FROM
+            notifications
+            WHERE
+          user_id = $1
+          LIMIT $2
+          OFFSET $3
+        `,
+        [userId, limit, offset]
+      )
+
+      return {
+        total_counts: rawNotifications[0].total_counts,
+        notifications: rawNotifications.map((notification) => ({
+          id: notification.id,
+          title: notification.title,
+          isRead: notification.is_read,
+          notificationType: notification.notification_type,
+          createdAt: notification.created_at,
+          updatedAt: notification.updated_at,
+        })),
+      }
+    } catch (e) {
+      throw new RepositoryError(
+        'NotificationRepository findAndCountAll error',
+        e as Error
+      )
+    }
+  }
+}
