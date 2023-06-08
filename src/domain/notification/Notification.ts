b3fb7622d1a2e5fbd010f4c5d528bfaef3431b8b
Ellen <ellen31725@gmail.com>
2023-06-08 23:04:24 +0800

    feat: add notification domain

---
+++ src/domain/notification/Notification.ts
@@ -0,0 +1,60 @@
+import { User } from '../user/User'
+
+export interface INotificationProps {
+  id: string
+  isRead: boolean
+  title: string
+  content: string
+  notificationType: NotificationType
+  createdAt: Date
+  updatedAt: Date
+  user: User // 自己才可以看到自己的通知
+}
+
+export enum NotificationType {
+  APPOINTMENT_NOTIFICATION = 'APPOINTMENT_NOTIFICATION',
+  HEALTH_GOAL_NOTIFICATION = 'HEALTH_GOAL_NOTIFICATION',
+  GET_ANSWER_NOTIFICATION = 'GET_ANSWER_NOTIFICATION',
+  THANK_YOU_NOTIFICATION = 'THANK_YOU_NOTIFICATION',
+  AGREED_NOTIFICATION = 'AGREED_NOTIFICATION',
+}
+
+export class Notification {
+  constructor(private readonly props: INotificationProps) {}
+
+  public get id(): string {
+    return this.props.id
+  }
+
+  public get isRead(): boolean {
+    return this.props.isRead
+  }
+
+  public get title(): string {
+    return this.props.title
+  }
+
+  public get content(): string {
+    return this.props.content
+  }
+
+  public get notificationType(): NotificationType {
+    return this.props.notificationType
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt
+  }
+
+  public get updatedAt(): Date {
+    return this.props.updatedAt
+  }
+
+  public get user(): User {
+    return this.props.user
+  }
+
+  public updateIsRead(isRead: boolean): void {
+    this.props.isRead = isRead
+  }
+}
