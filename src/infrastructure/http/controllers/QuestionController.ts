ee4d0e8e16e52a6e7ee7b9c65737c9299bee0f13
Ellen <ellen31725@gmail.com>
2023-06-21 10:41:36 +0800

    feat: modify the error handling of question controller

--- src/infrastructure/http/controllers/QuestionController.ts
+++ src/infrastructure/http/controllers/QuestionController.ts
@@ -66,268 +66,176 @@ export class QuestionController implements IQuestionController {
     req: Request,
     res: Response
   ): Promise<Response> => {
-    try {
-      const request = {
-        comment: req.body.comment,
-        user: req.user as User,
-        answerId: req.params.id,
-      }
-      const result = await this.createAnswerAgreementUseCase.execute(request)
-
-      return res.status(200).json(result)
-    } catch (error) {
-      // TODO: move this to a middleware
-      return res.status(400).json({ message: (error as Error).message })
+    const request = {
+      comment: req.body.comment,
+      user: req.user as User,
+      answerId: req.params.id,
     }
+    const result = await this.createAnswerAgreementUseCase.execute(request)
+    return res.status(200).json(result)
   }
 
   public editAnswerAgreementComment = async (
     req: Request,
     res: Response
   ): Promise<Response> => {
-    try {
-      const request = {
-        comment: req.body.comment,
-        user: req.user as User,
-        answerAgreementId: req.params.id,
-      }
-      const result = await this.editAnswerAgreementCommentUseCase.execute(
-        request
-      )
-
-      return res.status(200).json(result)
-    } catch (error) {
-      // TODO: move this to a middleware
-      return res
-        .status(400)
-        .json({ message: 'edit answer agreement comment error' })
+    const request = {
+      comment: req.body.comment,
+      user: req.user as User,
+      answerAgreementId: req.params.id,
     }
+    const result = await this.editAnswerAgreementCommentUseCase.execute(request)
+    return res.status(200).json(result)
   }
 
   public createAnswerAppreciation = async (
     req: Request,
     res: Response
   ): Promise<Response> => {
-    try {
-      const request = { ...req.body, user: req.user }
-      const result = await this.createAnswerAppreciationUseCase.execute(request)
-
-      return res.status(200).json(result)
-    } catch (error) {
-      // TODO: move this to a middleware
-      return res.status(400).json({ message: (error as Error).message })
-    }
+    const request = { ...req.body, user: req.user }
+    const result = await this.createAnswerAppreciationUseCase.execute(request)
+    return res.status(200).json(result)
   }
 
   public editAnswerAppreciationContent = async (
     req: Request,
     res: Response
   ): Promise<Response> => {
-    try {
-      const request = {
-        content: req.body.content,
-        user: req.user as User,
-        answerAppreciationId: req.params.id,
-      }
-      const result = await this.editAnswerAppreciationContentUseCase.execute(
-        request
-      )
-
-      return res.status(200).json(result)
-    } catch (error) {
-      // TODO: move this to a middleware
-      return res
-        .status(400)
-        .json({ message: 'edit answer appreciation content error' })
+    const request = {
+      content: req.body.content,
+      user: req.user as User,
+      answerAppreciationId: req.params.id,
     }
+    const result = await this.editAnswerAppreciationContentUseCase.execute(
+      request
+    )
+    return res.status(200).json(result)
   }
 
   public createPatientQuestionAnswer = async (
     req: Request,
     res: Response
   ): Promise<Response> => {
-    try {
-      const request = {
-        content: req.body.content,
-        user: req.user as User,
-        patientQuestionId: req.params.id,
-      }
-      const result = await this.createPatientQuestionAnswerUseCase.execute(
-        request
-      )
-
-      return res.status(200).json(result)
-    } catch (error) {
-      // TODO: move this to a middleware
-      return res.status(400).json({ message: (error as Error).message })
+    const request = {
+      content: req.body.content,
+      user: req.user as User,
+      patientQuestionId: req.params.id,
     }
+    const result = await this.createPatientQuestionAnswerUseCase.execute(
+      request
+    )
+    return res.status(200).json(result)
   }
 
   public editPatientQuestionAnswerContent = async (
     req: Request,
     res: Response
   ): Promise<Response> => {
-    try {
-      const request = {
-        content: req.body.content,
-        user: req.user as User,
-        patientQuestionAnswerId: req.params.id,
-      }
-      const result = await this.editPatientQuestionAnswerContentUseCase.execute(
-        request
-      )
-
-      return res.status(200).json(result)
-    } catch (error) {
-      // TODO: move this to a middleware
-      return res
-        .status(400)
-        .json({ message: 'edit patient question answer content error' })
+    const request = {
+      content: req.body.content,
+      user: req.user as User,
+      patientQuestionAnswerId: req.params.id,
     }
+    const result = await this.editPatientQuestionAnswerContentUseCase.execute(
+      request
+    )
+    return res.status(200).json(result)
   }
 
   public createPatientQuestion = async (
     req: Request,
     res: Response
   ): Promise<Response> => {
-    try {
-      const request = { ...req.body, user: req.user }
-      const result = await this.createPatientQuestionUseCase.execute(request)
-
-      return res.status(200).json(result)
-    } catch (error) {
-      // TODO: move this to a middleware
-      return res.status(400).json({ message: (error as Error).message })
-    }
+    const request = { ...req.body, user: req.user }
+    const result = await this.createPatientQuestionUseCase.execute(request)
+    return res.status(200).json(result)
   }
 
   public editPatientQuestion = async (
     req: Request,
     res: Response
   ): Promise<Response> => {
-    try {
-      const request = {
-        content: req.body.content,
-        medicalSpecialty: req.body.medicalSpecialty,
-        user: req.user as User,
-        patientQuestionId: req.params.id,
-      }
-      const result = await this.editPatientQuestionUseCase.execute(request)
-
-      return res.status(200).json(result)
-    } catch (error) {
-      // TODO: move this to a middleware
-      return res.status(400).json({ message: 'edit patient question error' })
+    const request = {
+      content: req.body.content,
+      medicalSpecialty: req.body.medicalSpecialty,
+      user: req.user as User,
+      patientQuestionId: req.params.id,
     }
+    const result = await this.editPatientQuestionUseCase.execute(request)
+    return res.status(200).json(result)
   }
 
   public cancelAnswerAppreciation = async (
     req: Request,
     res: Response
   ): Promise<Response> => {
-    try {
-      const request = {
-        answerAppreciationId: req.params.id,
-        user: req.user as User,
-      }
-      const result = await this.cancelAnswerAppreciationUseCase.execute(request)
-
-      return res.status(200).json(result)
-    } catch (error) {
-      // TODO: move this to a middleware
-      return res.status(400).json({ message: (error as Error).message })
+    const request = {
+      answerAppreciationId: req.params.id,
+      user: req.user as User,
     }
+    const result = await this.cancelAnswerAppreciationUseCase.execute(request)
+    return res.status(200).json(result)
   }
 
   public cancelAnswerAgreement = async (
     req: Request,
     res: Response
   ): Promise<Response> => {
-    try {
-      const request = {
-        answerAgreementId: req.params.id,
-        user: req.user as User,
-      }
-      const result = await this.cancelAnswerAgreementUseCase.execute(request)
-
-      return res.status(200).json(result)
-    } catch (error) {
-      // TODO: move this to a middleware
-      return res.status(400).json({ message: (error as Error).message })
+    const request = {
+      answerAgreementId: req.params.id,
+      user: req.user as User,
     }
+    const result = await this.cancelAnswerAgreementUseCase.execute(request)
+    return res.status(200).json(result)
   }
 
   public cancelPatientQuestionAnswer = async (
     req: Request,
     res: Response
   ): Promise<Response> => {
-    try {
-      const request = {
-        answerId: req.params.id,
-        user: req.user as User,
-      }
-      const result = await this.cancelPatientQuestionAnswerUseCase.execute(
-        request
-      )
-
-      return res.status(200).json(result)
-    } catch (error) {
-      // TODO: move this to a middleware
-      return res.status(400).json({ message: (error as Error).message })
+    const request = {
+      answerId: req.params.id,
+      user: req.user as User,
     }
+    const result = await this.cancelPatientQuestionAnswerUseCase.execute(
+      request
+    )
+    return res.status(200).json(result)
   }
 
   public cancelPatientQuestion = async (
     req: Request,
     res: Response
   ): Promise<Response> => {
-    try {
-      const request = {
-        patientQuestionId: req.params.id,
-        user: req.user as User,
-      }
-      const result = await this.cancelPatientQuestionUseCase.execute(request)
-
-      return res.status(200).json(result)
-    } catch (error) {
-      // TODO: move this to a middleware
-      return res.status(400).json({ message: (error as Error).message })
+    const request = {
+      patientQuestionId: req.params.id,
+      user: req.user as User,
     }
+    const result = await this.cancelPatientQuestionUseCase.execute(request)
+    return res.status(200).json(result)
   }
 
   public getSingleQuestion = async (
     req: Request,
     res: Response
   ): Promise<Response> => {
-    try {
-      const request = {
-        patientQuestionId: req.params.id,
-        user: req.user as User,
-      }
-      const result = await this.getSingleQuestionUseCase.execute(request)
-
-      return res.status(200).json(result)
-    } catch (error) {
-      // TODO: move this to a middleware
-      return res.status(400).json({ message: (error as Error).message })
+    const request = {
+      patientQuestionId: req.params.id,
+      user: req.user as User,
     }
+    const result = await this.getSingleQuestionUseCase.execute(request)
+    return res.status(200).json(result)
   }
 
   public getQuestions = async (
     req: Request,
     res: Response
   ): Promise<Response> => {
-    try {
-      const request = {
-        limit: Number(req.query.limit),
-        page: Number(req.query.page),
-      }
-      const result = await this.getQuestionsUseCase.execute(request)
-
-      return res.status(200).json(result)
-    } catch (error) {
-      // TODO: move this to a middleware
-      return res.status(400).json({ message: (error as Error).message })
+    const request = {
+      limit: Number(req.query.limit),
+      page: Number(req.query.page),
     }
+    const result = await this.getQuestionsUseCase.execute(request)
+    return res.status(200).json(result)
   }
 }
