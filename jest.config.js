4fa7ac0ba3c699942e38f1a40944f7fd83ab1e76
Elek.Chen <>
2023-06-29 21:34:38 +0800

    add test db for integration test

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/build/',
+    '/migrations/',
+    'ormconfig-cli.ts',
+    '.*/tests/(.*/)*.+.ts$',
+    '/.*.integration.test.ts$/',
+  ],
 };
