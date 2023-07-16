aee3cdb897b2bb72d538807453880cb5b99de4c5
Ellen <ellen31725@gmail.com>
2023-07-17 01:52:01 +0800

    fix: modify authenticated to routes

--- src/infrastructure/http/routes/ConsultationRoutes.ts
+++ src/infrastructure/http/routes/ConsultationRoutes.ts
@@ -8,7 +8,9 @@ import {
   createDoctorTimeSlotSchema,
   createMultipleTimeSlotsSchema,
   editDoctorTimeSlotSchema,
+  getDoctorTimeSlotsSchema,
 } from '../../../application/consultation/ConsultationValidator'
+import { authenticated } from '../middlewares/Auth'
 
 export class ConsultationRoutes {
   private readonly routes: Router
@@ -19,36 +21,49 @@ export class ConsultationRoutes {
     this.routes
       .delete(
         '/:id',
+        authenticated,
         validator(cancelConsultAppointmentSchema),
         asyncHandler(this.consultationController.cancelConsultAppointment)
       )
       .post(
         '/time-slot',
+        authenticated,
         validator(createDoctorTimeSlotSchema),
         asyncHandler(this.consultationController.createDoctorTimeSlot)
       )
       .patch(
         '/time-slot/:id',
+        authenticated,
         validator(editDoctorTimeSlotSchema),
         asyncHandler(this.consultationController.editDoctorTimeSlot)
       )
       .post(
         '/multiple-time-slots',
+        authenticated,
         validator(createMultipleTimeSlotsSchema),
         asyncHandler(this.consultationController.createMultipleTimeSlots)
       )
       .post(
         '/',
+        authenticated,
         validator(createConsultAppointmentSchema),
         asyncHandler(this.consultationController.createConsultAppointment)
       )
       .get(
         '/patient',
+        authenticated,
         asyncHandler(this.consultationController.getPatientConsultAppointments)
       )
       .get(
         '/doctor',
+        authenticated,
         asyncHandler(this.consultationController.getDoctorConsultAppointments)
+      )
+      .get(
+        '/time-slots/doctors/:id',
+        authenticated,
+        validator(getDoctorTimeSlotsSchema),
+        asyncHandler(this.consultationController.getDoctorTimeSlots)
       )
   }
 
