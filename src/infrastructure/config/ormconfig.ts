9be2b387ea0b2d2fab8446b5a3e136e47f191ea4
Elek.Chen <>
2023-08-29 20:03:17 +0800

    fix: compile errors

--- src/infrastructure/config/ormconfig.ts
+++ src/infrastructure/config/ormconfig.ts
@@ -1,6 +1,14 @@
 import { DataSourceOptions } from 'typeorm'
 import 'dotenv/config'
 
+const isDevMode = process.env.TS_NODE === 'true'
+
+const entities = isDevMode
+  ? ['./src/infrastructure/entities/**/*Entity.ts']
+  : ['./build/infrastructure/entities/**/*Entity.js']
+
+const migrations = isDevMode ? ['./migrations/*.ts'] : []
+
 const OrmConfig: DataSourceOptions = {
   type: 'postgres',
   host: process.env.POSTGRES_HOST,
@@ -10,9 +18,10 @@ const OrmConfig: DataSourceOptions = {
   database: process.env.POSTGRES_DB_NAME,
   synchronize: false,
   logging: true,
-  entities: ['./src/infrastructure/entities/**/*Entity.ts'],
-  migrations: ['./migrations/*.ts'],
+  entities,
+  migrations,
   migrationsTableName: 'migrations',
+  // ssl: frue,
 }
 
 export default function getOrmConfig(): DataSourceOptions {
