90e18510626da6bc65a68ec3472172a24477c37b
Ellen <ellen31725@gmail.com>
2023-09-16 18:38:42 +0800

    fix: modify raw sql to count thanks

--- src/infrastructure/entities/question/PatientQuestionAnswerRepository.ts
+++ src/infrastructure/entities/question/PatientQuestionAnswerRepository.ts
@@ -255,10 +255,14 @@ export class PatientQuestionAnswerRepository
     try {
       const rawCounts = await this.getQuery<Array<{ count: number }>>(
         `
-        SELECT COUNT(DISTINCT patient_question_answers.id) as count
-        FROM patient_question_answers
-        JOIN answer_appreciations ON patient_question_answers.id = answer_appreciations.answer_id AND answer_appreciations.deleted_at IS NULL
-        WHERE patient_question_answers.doctor_id = $1
+        SELECT
+          COUNT(DISTINCT answer_appreciations.id) as "count"
+        FROM
+          patient_question_answers
+        LEFT JOIN
+          answer_appreciations ON patient_question_answers.id = answer_appreciations.answer_id AND answer_appreciations.deleted_at IS NULL
+        WHERE
+          patient_question_answers.doctor_id = $1;
       `,
         [doctorId]
       )
